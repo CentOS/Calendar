bfb01a3e71c1bc4c8e6d84d945f40526b4379609
rbowen <rbowen@rcbowen.com>
2019-12-04 13:03:30 -0500

    Link to actual location of ical file. Fix cloud-sig data.

---
+++ doit.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+yaml2ical -y meetings -o output/irc-meetings.ical -t list.markdown.jinja -w output/calendar.md -f -n "CentOS Meetings" -d "Meeting schedule for the CentOS Project and SIGS"
