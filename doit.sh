1ac097abc068d63a83673a37fb147d28a4c488a3
rbowen <rbowen@rcbowen.com>
2021-01-21 09:09:30 -0500

    A little less work

--- doit.sh
+++ doit.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 yaml2ical -y meetings -o output/irc-meetings.ical -t list.markdown.jinja -w output/calendar.md -f -n "CentOS Meetings" -d "Meeting schedule for the CentOS Project and SIGS"
+cp ./output/* ../centos.org/community/
+
+
